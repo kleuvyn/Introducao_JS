78b23bbeca4cafedcdd44241101d4986b0b1a718
kleuvyn <kleuvyn@hotmail.com>
2023-09-26 23:50:19 -0300

    Aula 6 – Tratamento de dados

---
+++ Aula 6 – Tratamento de dados.js
@@ -0,0 +1,28 @@
+// Aula 02 variavel
+
+// var nome = window.prompt('Qual é o seu nome?');
+// window.alert(' É um grande prazer te conhecer, ' + nome + '!');
+
+// Aula 03 String = Número
+
+// var numero01 = Number(window.prompt('Digite um número: '));
+// var numero02 = Number(window.prompt('Digite outro número: '));
+
+// var numero01 = Number.parseFloat(window.prompt('Digite um número: '));
+// var numero02 = Number.parseFloat(window.prompt('Digite outro número: '));
+
+// var numero01 = Number.parseInt(window.prompt('Digite um número: '));
+// var numero02 = Number.parseInt(window.prompt('Digite outro número: '));
+
+// var soma = numero01 + numero02
+
+// window.alert('A soma dos valores é: ' + soma);
+// window.alert(`A soma entre ${numero01} e ${numero02} é igual a ${soma}`);
+
+// Número = String
+// window.alert('A soma dos valores é: ' + soma.toString ());
+// window.alert('A soma dos valores é: ' + String(soma));
+
+
+
+
