f315d0b28bd92321d84bbaa9d522409ef930a72b
kleuvyn <kleuvyn@hotmail.com>
2024-03-20 11:27:18 -0300

    vendo JSON

---
+++ 009- Estrutura de Dados JSON/arrayJSON.js
@@ -0,0 +1,20 @@
+//Array JSON: É uma lista ordenada de valores, onde cada valor pode ser de qualquer tipo de dado JSON válido, incluindo outros objetos ou arrays. 
+
+let frutas = [
+    {
+      "nome": "maçã",
+      "cor": "vermelha",
+      "sabor": "azedo"
+    },
+    {
+      "nome": "laranja",
+      "cor": "laranja",
+      "sabor": "azedo"
+    },
+    {
+      "nome": "banana",
+      "cor": "amarela",
+      "sabor": "doce"
+    }
+  ];
+  
---
+++ 009- Estrutura de Dados JSON/objetoJSON.js
@@ -0,0 +1,20 @@
+// Objeto JSON: É uma coleção de pares chave-valor, onde as chaves são strings e os valores podem ser de qualquer tipo de dado JSON válido, incluindo outros objetos ou arrays. 
+let pessoa = {
+  "nome": "Beatryz",
+  "idade": 29,
+  "cidade": "Rio de Janeiro",
+  "emails": ["beatrtyz@example.com", "beatryz.work@example.com"],
+  "telefones": {
+    "celular": "123456789",
+    "fixo": "987654321"
+  },
+  "trabalho": {
+    "cargo": "Desenvolvedora",
+    "empresa": "ABC Tech",
+    "endereco": {
+      "rua": "Rua ABC",
+      "numero": 123,
+      "cidade": "Rio de Janeiro"
+    }
+  }
+};
